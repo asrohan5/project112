bdab2d695ad3f0918f0f14b178e07a25a49899cb
asrohan5 <rohanrohu99@gmail.com>
2026-02-04 23:33:54 +0530

    advanced model

--- src/main.py
+++ src/main.py
@@ -1,89 +1,88 @@
 import logging
+import pandas as pd
 from src.ingestion import load_and_optimize_data
-from src.metrics import *
+from src.metrics import clean_data_for_metrics, calculate_rfm_values, assign_rfm_scores, define_customer_segments, calculate_cohort_index, get_retention_matrix, build_executive_summary
 from src import vizualization as viz
-from src import features 
+from src import features
 from src import eda
-from src.data_factory import *
+from src.data_factory import create_churn_dataset
 from src import model as ml_engine
+from src import model_advanced
 
-logging.basicConfig(level=logging.INFO, format = "%(asctime)s [%(levelname)s] %(message)s")
+
+RUN_VIZ_AND_REPORTING = False
+RUN_FEATURE_ENGG = False
+RUN_TARGET_GEN = False
+RUN_BASELINE_MODEL = False
+RUN_ADVANCED_MODEL = True
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s %(message)s]")
 
 def run_pipeline():
 
-    df_raw = load_and_optimize_data()
-    if df_raw is None:
-        logging.error('Pipeline failed: Could not load data')
-        return
+    if RUN_VIZ_AND_REPORTING or RUN_FEATURE_ENGG or RUN_TARGET_GEN:
+        df_raw = load_and_optimize_data()
+        if df_raw is None:
+            logging.error('Pipeline Failed: Could not load data')
+            return
+        df_clean = clean_data_for_metrics(df_raw)
+    else:
+        logging.info("Skipping Raw Data Load")
 
-    df = clean_data_for_metrics(df_raw)
-
-    logging.info('Generating RFM Segments')
-    rfm = calculate_rfm_values(df)
-    rfm = assign_rfm_scores(rfm)
-    rfm = define_customer_segments(rfm)
-
-
-    logging.info('Calculating Cohort Retention')
-    df_cohort = calculate_cohort_index(df)
-    retention = get_retention_matrix(df_cohort)
-
-
-    logging.info('Building Executive Summary')
-    exec_summary = build_executive_summary(df)
-
-
-    logging.info('Preparing Vizualizations')
-    viz.plot_retention_heatmap(retention)
-    viz.plot_segement_distribution(rfm)
-    viz.plot_revenue_growth(exec_summary)
-
-    logging.info('Pipeline Execution Completed')
-
-
-
-    logging.info("Engineering Predictive Features...")
-    feature_matrix = features.engineer_features(df, df_raw)
     
+    if RUN_VIZ_AND_REPORTING:
+        logging.info('::: RUNNING REPORTING LAYER :::')
+        rfm = calculate_rfm_values(df_clean)
+        rfm = assign_rfm_scores(rfm)
+        rfm = define_customer_segments(rfm)
 
-    print("\n--- Day 6 Feature Matrix Sample ---")
-    print(feature_matrix[['spend_velocity', 'return_rate', 'whale_score']].head())
-    print(f"Total Features Generated: {feature_matrix.shape[1]}")
-    print("\n\n")
+        df_cohort = calculate_cohort_index(df_clean)
+        retention = get_retention_matrix(df_cohort)
+        exec_summary = build_executive_summary(df_clean)
 
+        viz.plot_retention_heatmap(retention)
+        viz.plot_segement_distribution(rfm)
+        viz.plot_revenue_growth(exec_summary)
 
-    feature_matrix.to_csv("outputs/feature_matrix.csv")
-
-
-    # ... (Load and Clean Data steps) ...
-    df_raw = load_and_optimize_data()
-    df_clean = clean_data_for_metrics(df_raw)
-
-
-    logging.info("Creating Training Set with Time-Split...")
     
+    if RUN_FEATURE_ENGG:
+        logging.info(" ::: RUNNING FE ::: ")
+        feature_matrix = features.engineer_features(df_clean, df_raw)
 
-    training_cutoff = pd.Timestamp('2011-09-01')
+        print(feature_matrix[['spend_velocity', 'return_rate', 'whale_score']].head())
+        feature_matrix.to_csv('outputs/feature_matrix.csv')
+
+    if RUN_TARGET_GEN:
+        logging.info(" ::: Creating Training Set :::")
+        training_cutoff = pd.TimeStamp('2011-09-01')
+        train_df = create_churn_dataset(df_clean, df_raw, cutoff_date=training_cutoff)
+
+        train_df.to_csv('outputs/modeling_data.csv')
+        eda.plot_churn_separation(train_df)
     
-    train_df = create_churn_dataset(df_clean, df_raw, cutoff_date=training_cutoff)
+    if RUN_BASELINE_MODEL or RUN_ADVANCED_MODEL:
+        try: 
+            logging.info("Loading Pre-Processed modeling data...")
+            df_model = pd.read_csv('outputs/modeling_data.csv', index_col=0)
+
+        except FileNotFoundError:
+            logging.error('CRITICAL: modeling_data.csv not found. Set RUN_TARGET_GEN = True')
+            return
     
+        if RUN_BASELINE_MODEL:
+            logging.info('Training Baseline Logistic Regression...')
+            trained_model, y_test, y_pred, feat_imp = ml_engine.train_baseline_model(df_model)
+            ml_engine.plot_confusion_matrix(y_test, y_pred)
 
-    train_df.to_csv("outputs/modeling_data.csv")
     
-
-    print("\n--- Correlation Analysis ---")
-    eda.plot_churn_separation(train_df)
-
-
-    try:
-        df_model = pd.read_csv('outputs/modeling_data.csv', index_col=0)
-    except FileNotFoundError:
-        print('Error: Run other steps first to generate modeling_data.csv')
-        return
+        if RUN_ADVANCED_MODEL:
+            logging.info('Tarining Advanced Random Forest (Grid Search)...')
+            rf_model, feature_names = model_advanced.train_random_forest(df_model)
+            model_advanced.plot_feature_importance_rf(rf_model, feature_names)
     
-    trained_model, y_test, y_pred, feat_imp = ml_engine.train_baseline_model(df_model)
+    logging.info('+++++ Pipeline Execution Completed Successfully +++++')
 
-    ml_engine.plot_confusion_matrix(y_test=y_test, y_pred=y_pred)
 
 if __name__ == "__main__":
     run_pipeline()
---
+++ src/model_advanced.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import classification_report, roc_auc_score
+
+def train_random_forest(df):
+    print('::: Training Random Forest (advanced) :::')
+    features = df.drop(columns=['is_churned', 'spend_last_30d', 'spend_prior_30d'])
+    target = df['is_churned']
+
+    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+
+
+    param_grid = {'n_estimators':[50,100,200],
+                  'max_depth': [5,10,20],
+                  'min_samples_split': [2,10]}
+
+    
+    rf = RandomForestClassifier(random_state=42)
+    grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=3, scoring='roc_auc', verbose=1)
+
+    print('Tuning Hyperparameters....')
+    grid_search.fit(X_train, y_train)
+
+    best_model = grid_search.best_estimator_
+    print(f'Best Parameters: {grid_search.best_params_}')
+
+    y_pred = best_model.predict(X_test)
+    y_prob = best_model.predict_proba(X_test)[:,1]
+
+    print('\n Random Forest Report: ')
+    print(classification_report(y_test, y_pred))
+    print(f'New ROC-AUC Score: {roc_auc_score(y_test, y_prob):.4f}')
+
+    return best_model, features.columns
+
+def plot_feature_importance_rf(model, feature_names):
+
+    importances = model.feature_importances_
+    indices = np.argsort(importances)[::-1]
+
+    plt.figure(figsize=(10,6))
+    plt.title("Feature Importance (Random FOrest)")
+    plt.bar(range(len(importances)), importances[indices], align="center", color='forestgreen')
+    plt.xticks(range(len(importances)), [feature_names[i] for i in indices], rotation=45, ha='right')
+    plt.tight_layout()
+    plt.savefig('plots/6_feature_importance.png')
+
